163105c22a3a294cf1fe9bc72b3e3f2fa1582e38
AlaaMahi <60281034+AlaaMahi@users.noreply.github.com>
2020-03-09 01:01:20 +0000

    Create 7.go

---
+++ 7.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+	arrayLength, _ := strconv.Atoi(readInputLine())
+	valueArrayString := strings.Split(readInputLine(), " ")
+	var result string
+	for i := arrayLength - 1; i > -1; i-- {
+		result += (valueArrayString[i] + " ")
+	}
+	fmt.Println(result)
+}
+
+func readInputLine() string {
+	text, _ := reader.ReadString('\n')
+	return strings.TrimSuffix(text, "\n")
+}
