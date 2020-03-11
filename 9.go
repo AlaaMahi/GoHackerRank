c0db2e0044d530e2836c2b19bf20ffb68c760b81
AlaaMahi <60281034+AlaaMahi@users.noreply.github.com>
2020-03-11 16:31:10 +0000

    Create 9.go

---
+++ 9.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+	n, _ := strconv.Atoi(readInputLine())
+	fmt.Println(factorial(n))
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	} else {
+		return n * factorial(n-1)
+	}
+}
+
+func readInputLine() string {
+	text, _ := reader.ReadString('\n')
+	return strings.TrimSuffix(text, "\n")
+}
